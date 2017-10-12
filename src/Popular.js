818c8c6a9593399e973b4037e69cf8dcef02d348
luisaGonzales <luisagc711@gmail.com>
2017-10-12 14:09:52 -0500

    Vista Popular

--- src/Popular.js
+++ src/Popular.js
@@ -7,8 +7,14 @@ import ReactBootstrap from 'react-bootstrap'
 const Popular = ({model}) => {
     return (
         <div>
-            <h1>Popular</h1>
-            
+            <ul className="languages">
+                <li>All</li>
+                <li>Javascript</li>
+                <li>Ruby</li>
+                <li>Java</li>
+                <li>CSS</li>
+                <li>Python</li>
+            </ul>
         </div>
     );
 }
