7edf9f4d360c02a8d9cdf561a4d3a03f243004ae
luisaGonzales <luisagc711@gmail.com>
2017-10-12 14:04:24 -0500

    Vista Battle

--- src/Battle.js
+++ src/Battle.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Battle.css';
-import {Button} from 'react-bootstrap';
+import {
+    Button,
+    Grid,
+    Row,
+    Col
+
+} from 'react-bootstrap';
 import ReactBootstrap from 'react-bootstrap'
 
 const Battle = () => (
     <div>
-        <h1>Battle</h1>
+        <Grid className="text-center">
+            <Row>
+                <Col xs={6} md={6} >
+                    <form className="colum">
+                        <label htmlFor="username" className="header text-center" >Player One</label>
+                        <input className="inputPlayers" type="text" id="username" placeholder="GitHub User Name" autoComplete="off"/>
+                        <button className="button" type="submit">Submit</button>
+                    </form>
+                </Col>
+                <Col xs={6} md={6} >
+                    <form className="colum">
+                        <label htmlFor="username" className="header text-center" >Player Two</label>
+                        <input className="inputPlayers" type="text" id="username" placeholder="GitHub User Name" autoComplete="off"/>
+                        <button className="button" type="submit">Submit</button>
+                    </form>
+                </Col>
+            </Row>
+        </Grid>
     </div>
   );
   
